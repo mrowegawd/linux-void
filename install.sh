ca54f9cdf1980b2115bb3e7c95b7ce2821dde717
mrowegawd <[7404751+mrowegawd@users.noreply.github.com](mailto:7404751+mrowegawd@users.noreply.github.com)>
2021-12-26 22:45:32 +0700

    Test dotfiles

--- install.sh
+++ install.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-# TODO: install paramiko atau python3 untuk menjalankan minimal ansible
-echo "yes"
+# TODO: install paramiko, python3 untuk menjalankan minimal ansible
+# echo "yes"
+
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install --no-install-recommends -y python3-pip \
+	python-pip \
+	python3-dev \
+	gnupg \
+	gnupg2 \
+	unzip \
+	wget \
+	git \
+	vim \
+	stow \
+	build-essential \
+	libssl-dev \
+	libffi-dev \
+	sshpass \
+	python-apt \
+	python-setuptools \
+	python3-setuptools
+
+sudo -H pip install ansible
+sudo -H pip install paramiko
