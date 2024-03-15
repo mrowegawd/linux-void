f37cf47825368ac96d9b3a6692a28853aa966647
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-03-16 05:13:34 +0700

    fix: day 2

--- install.sh
+++ install.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
-# TODO: install paramiko, python3 untuk menjalankan minimal ansible
-
 sudo apt update -y && sudo apt upgrade -y
 
 sudo apt install --no-install-recommends -y \
-  gnupg \
-  gnupg2 \
-  unzip \
-  wget \
-  git \
-  vim \
-  stow \
-  build-essential \
-  libssl-dev \
-  libffi-dev \
-  sshpass \
-  python3-pip \
+	gnupg \
+	gnupg2 \
+	unzip \
+	wget \
+	git \
+	vim \
+	stow \
+	build-essential \
+	libssl-dev \
+	libffi-dev \
+	sshpass \
+	python3-pip
 
+# Install paramiko, python3 untuk menjalankan minimal ansible
 sudo pip3 install -U ansible
 sudo pip3 install -U paramiko
 
 _mkpass() {
-  read -r -p "your password? " getvar
-  # read -r "getvar?your password? "
+	read -r -p "your password? " getvar
 
-  mkdir -p tmp
+	mkdir -p tmp
 
-  mkpasswd -m sha-512 "$getvar" > tmp/userpass-SHA5
-  echo "$getvar" > tmp/userpass
-  echo "$USER" > tmp/user
+	mkpasswd -m sha-512 "$getvar" >tmp/userpass-SHA5
+	echo "$getvar" >tmp/userpass
+	echo "$USER" >tmp/user
 }
 
 if [ -z "$(command -v mkpasswd)" ]; then
-  echo "package whois not found! let me install it.."
-  sudo apt install whois -y
-  sleep 1
+	echo "package whois not found! let me install it.."
+	sudo apt install whois -y
+	sleep 1
 fi
 
 clear
