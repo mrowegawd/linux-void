77b97549e100ecd55bf99885aa8067a737051624
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2024-07-26 05:28:23 +0700

    update qemu, tools, add `tool-misc`

--- install.sh
+++ install.sh
@@ -3,37 +3,37 @@
 sudo apt update -y && sudo apt upgrade -y
 
 sudo apt install --no-install-recommends -y \
-	gnupg \
-	gnupg2 \
-	unzip \
-	wget \
-	git \
-	vim \
-	stow \
-	build-essential \
-	libssl-dev \
-	libffi-dev \
-	sshpass \
-	python3-pip
+  gnupg \
+  gnupg2 \
+  unzip \
+  wget \
+  git \
+  vim \
+  stow \
+  build-essential \
+  libssl-dev \
+  libffi-dev \
+  sshpass \
+  python3-pip
 
 # Install paramiko, python3 untuk menjalankan minimal ansible
 sudo pip3 install -U ansible
 sudo pip3 install -U paramiko
 
 _mkpass() {
-	read -r -p "your password? " getvar
+  read -r -p "your password? " getvar
 
-	mkdir -p tmp
+  mkdir -p tmp
 
-	mkpasswd -m sha-512 "$getvar" >tmp/userpass-SHA5
-	echo "$getvar" >tmp/userpass
-	echo "$USER" >tmp/user
+  mkpasswd -m sha-512 "$getvar" >tmp/userpass-SHA5
+  echo "$getvar" >tmp/userpass
+  echo "$USER" >tmp/user
 }
 
 if [ -z "$(command -v mkpasswd)" ]; then
-	echo "package whois not found! let me install it.."
-	sudo apt install whois -y
-	sleep 1
+  echo "package whois not found! let me install it.."
+  sudo apt install whois -y
+  sleep 1
 fi
 
 clear
@@ -44,3 +44,5 @@ sleep 2
 printf "[+] Starting ansible-playbook !!\n\n"
 
 ansible-playbook install-fresh.yml --ask-become-pass
+
+# ansible-galaxy collection install community.libvirt
