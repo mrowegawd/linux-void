d61b683d9ad935dad538b4c97b73163d84c93470
mrowegawd <7404751+mrowegawd@users.noreply.github.com>
2025-05-07 23:07:47 +0700

    feat(roles): update Golang version and refactor installation script
    - Updated Golang version in `group_vars/all/programming.yml` from 1.22.5 to 1.24.2.
    - Refactored `install.sh`:
      - Modularized the script by introducing `__install_deps` and `playbook_start` functions.
      - Added `pipx` for managing Python tools and installed `ansible-dev-tools`, `molecule`, and `molecule-plugins[docker]`.
      - Improved script structure with a `main` function to handle different execution modes (`all` and `deps`).
    - Updated `pyproject.toml`:
      - Changed `[tool.poetry.dev-dependencies]` to `[tool.poetry.group.dev.dependencies]` for better dependency grouping.

--- install.sh
+++ install.sh
@@ -1,60 +1,78 @@
 #!/bin/bash
 
-sudo apt update -y && sudo apt upgrade -y
+__install_deps() {
+  sudo apt update -y && sudo apt upgrade -y
+  sudo apt install --no-install-recommends -y \
+    gnupg \
+    gnupg2 \
+    unzip \
+    wget \
+    git \
+    vim \
+    stow \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    sshpass \
+    python3-pip
 
-sudo apt install --no-install-recommends -y \
-  gnupg \
-  gnupg2 \
-  unzip \
-  wget \
-  git \
-  vim \
-  stow \
-  build-essential \
-  libssl-dev \
-  libffi-dev \
-  sshpass \
-  python3-pip
+  pip install pipx
 
-# Install paramiko, python3 untuk menjalankan minimal ansible
-sudo pip3 install -U ansible
-sudo pip3 install -U paramiko
-
-WORKDIR="$HOME/moxconf/development"
-CURRENT_DIR="$(pwd)"
-
-mkdir -p "$WORKDIR"
-
-# Periksa apakah directory saat ini sesuai dengan directory yang ditentukan
-if [ "$CURRENT_DIR" != "$TARGET_DIR" ]; then
-  echo "Error: the script must be run from directory $TARGET_DIR"
-  echo "go there and run or git clone again"
-  exit 1
-fi
-
-_mkpass() {
-  read -r -p "your password? " getvar
-
-  mkdir -p tmp
-
-  mkpasswd -m sha-512 "$getvar" >tmp/userpass-SHA5
-  echo "$getvar" >tmp/userpass
-  echo "$USER" >tmp/user
+  # https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible
+  pipx install ansible-dev-tools --include-devs
+  pipx install molecule --include-devs
+  pipx install "molecule-plugins[docker]" --include-deps # for driver
 }
 
-if [ -z "$(command -v mkpasswd)" ]; then
-  echo "package whois not found! let me install it.."
-  sudo apt install whois -y
-  sleep 1
-fi
+playbook_start() {
+  WORKDIR="$HOME/moxconf/development"
+  CURRENT_DIR="$(pwd)"
 
-clear
-echo ""
+  mkdir -p "$WORKDIR"
 
-_mkpass
-sleep 2
-printf "[+] Starting ansible-playbook !!\n\n"
+  # Periksa apakah directory saat ini sesuai dengan directory yang ditentukan
+  if [ "$CURRENT_DIR" != "$TARGET_DIR" ]; then
+    echo "Error: the script must be run from directory $TARGET_DIR"
+    echo "go there and run or git clone again"
+    exit 1
+  fi
 
-ansible-playbook install-fresh.yml --ask-become-pass
+  _mkpass() {
+    read -r -p "your password? " getvar
 
-# ansible-galaxy collection install community.libvirt
+    mkdir -p tmp
+
+    mkpasswd -m sha-512 "$getvar" >tmp/userpass-SHA5
+    echo "$getvar" >tmp/userpass
+    echo "$USER" >tmp/user
+  }
+
+  if [ -z "$(command -v mkpasswd)" ]; then
+    echo "package whois not found! let me install it.."
+    sudo apt install whois -y
+    sleep 1
+  fi
+
+  clear
+  echo ""
+
+  _mkpass
+  sleep 2
+  printf "[+] Starting ansible-playbook !!\n\n"
+
+  ansible-playbook install-fresh.yml --ask-become-pass
+
+}
+
+main() {
+  if [[ $1 == "all" ]]; then
+    __install_deps
+    playbook_start
+  fi
+
+  if [[ $1 == "deps" ]]; then
+    __install_deps
+  fi
+}
+
+"$@"
